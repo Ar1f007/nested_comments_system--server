023556696818cce03c2bff3c701b26f7a2ac36df
Ar1f007 <muhammadarif9416@yahoo.com>
2022-08-09 21:07:04 +0600

    user authentication

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ import fastify from 'fastify';
 import sensible from '@fastify/sensible';
 import dotenv from 'dotenv';
 import cors from '@fastify/cors';
+import cookie from '@fastify/cookie';
 import { PrismaClient } from '@prisma/client';
 dotenv.config();
 
 const app = fastify();
 app.register(sensible);
+app.register(cookie, { secret: process.env.COOKIE_SECRET });
 app.register(cors, {
   origin: process.env.CLIENT_URL,
   credentials: true,
 });
+
+app.addHook('onRequest', (req, res, done) => {
+  if (req.cookies.userId !== CURRENT_USER_ID) {
+    req.cookies.userId = CURRENT_USER_ID;
+    res.clearCookie('userId');
+    res.setCookie('userId', CURRENT_USER_ID);
+  }
+  done();
+});
+
 const prisma = new PrismaClient();
+
+const CURRENT_USER_ID = (await prisma.user.findFirst({ where: { name: 'John' } })).id;
 
 app.get('/posts', async (req, res) => {
   return await commitToDb(
@@ -48,6 +62,21 @@ app.get('/posts/:id', async (req, res) => {
           },
         },
       },
+    })
+  );
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const message = req.body?.message;
+  const parentId = req.body?.parentId;
+
+  if (message === '' || message === null) {
+    return res.send(app.httpErrors.badRequest('Message is required'));
+  }
+
+  return await commitToDb(
+    prisma.comment.create({
+      data: { message, userId: req.cookies.userId, parentId, postId: req.params.id },
     })
   );
 });
